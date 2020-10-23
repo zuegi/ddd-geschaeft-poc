6e75dd05c1bc24fed7a2525fca8c4b9336f2f968
Rene Weishaupt <rene.weishaupt@thurweb.ch>
2020-10-23 17:59:40 +0200

    Initial Commit

---
+++ src/main/java/com/github/zuegi/dddgeschaeftpoc/DddGeschaeftPocApplication.java
@@ -0,0 +1,13 @@
+package com.github.zuegi.dddgeschaeftpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddGeschaeftPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddGeschaeftPocApplication.class, args);
+    }
+
+}
